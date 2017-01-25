612cc1f3e60af5c560eecdb2b4f946658f9a56a2
todorkolev81 <todor.kolev81@gmail.com>
2017-01-26 00:32:37 +0200

    sf

---
+++ Drawing Figures with Loops/square frame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square_frame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("+");
+
+            for (var i = 0; i < n - 2; i++)
+            {
+                Console.Write(" -");
+            }
+            Console.WriteLine(" +");
+
+            for (var i = 0; i < n - 2; i++)
+            {
+                Console.Write("|");
+                for (var row = 0; row < n - 2; row++)
+                {
+                    Console.Write(" -");
+                }
+                Console.WriteLine(" |");
+            }
+            Console.Write("+");
+
+            for (var i = 0; i < n - 2; i++)
+            {
+                Console.Write(" -");
+            }
+            Console.WriteLine(" +");
+        }
+    }
+}
