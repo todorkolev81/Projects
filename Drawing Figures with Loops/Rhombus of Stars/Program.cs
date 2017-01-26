58dba97f868b5ca3d294dbee4444fae5efdd5006
todorkolev81 <todor.kolev81@gmail.com>
2017-01-26 08:13:00 +0200

    rs

---
+++ Drawing Figures with Loops/Rhombus of Stars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rhombus_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (var row = 1; row <= n; row++)
+            {
+                for (var col = 1; col <= n - row; col++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("*");
+
+                for (var col = 1; col < row; col++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
